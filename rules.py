21a7901bfaf70dca246fa32eb60cfb554ecdd23b
Shahriar Ahmed (Sazid) <shahriarahmedsazid@gmail.com>
2023-03-05 12:41:36 +0600

    homophones replace added

--- rules.py
+++ rules.py
@@ -1,9 +1,17 @@
-from itertools import combinations
+from itertools import combinations, permutations, product
+import re
 
-def remove_ith_char(s:str, idx):
-    return s[:idx] + s[idx+1:]
 
-def skip_letter(s:str, n=1):
+def remove_ith_char(s: str, idx):
+    return s[:idx] + s[idx + 1:]
+
+def replace_str_index(s,index, victim_length = 1, replacement=''):
+    if index >= len(s) or index < 0:
+        return s
+    
+    return f"{s[:index]}{replacement}{s[index+victim_length:]}"
+
+def skip_letter(s: str, n=1):
     length = len(s)
 
     miss_combinations = list(combinations([i for i in range(length)], n))
@@ -11,23 +19,25 @@ def skip_letter(s:str, n=1):
     typos = []
     for combination in miss_combinations:
         typo = s
-        for i in range(len(combination)-1, -1, -1):
-            typo = remove_ith_char(typo, combination[i]) 
+        for i in range(len(combination) - 1, -1, -1):
+            typo = remove_ith_char(typo, combination[i])
         typos.append(typo)
 
     return typos
 
-def reverse_letter(s:str):
+
+def reverse_letter(s: str):
     typos = []
 
-    for i in range(len(s)-1, 0, -1):
+    for i in range(len(s) - 1, 0, -1):
         typo = list(s)
-        typo[i-1], typo[i] = typo[i], typo[i-1]
+        typo[i - 1], typo[i] = typo[i], typo[i - 1]
         typos.append(''.join(typo))
-    
+
     return typos
 
-def double_letter(s:str):
+
+def double_letter(s: str):
     typos = []
 
     for i in range(len(s)):
@@ -35,25 +45,53 @@ def double_letter(s:str):
         typo.insert(i, typo[i])
         typos.append(''.join(typo))
 
-    
     return typos
 
-def keyboard_adjacent_letter(s:str):
+
+def keyboard_adjacent_letter(s: str):
     pass
 
-def vowel_replace(s:str):
+
+def vowel_replace(s: str):
     pass
+
 
 def homophones_replace(s: str):
-    homophones_dict = {
-        'j': ['z', 'g', ],
-        'a': ['o'],
-        'i': ['e'],
-        'c': ['s', 'k'],
-        'q': ['k'],
-        'u': ['o'],
-        's': ['sh']
-    }
+    typos = []
+    homophones_dict = (
+        ('j', 'z', 'g'),
+        ('a', 'o'),
+        ('i', 'e'),
+        ('c', 's', 'k'),
+        ('q', 'k'),
+        ('u', 'o', 'oo'),
+        ('f', 'p', 'ph'),
+        ('s', 'sh'),
+    )
 
-    pass
+    for homophone in homophones_dict:
+        index_dict = {}
+        for char in homophone:
+            idx_list = [m.start() for m in re.finditer(char,s)]
+            for idx in idx_list:
+                index_dict[idx] = {
+                    'index': idx,
+                    'length': len(char)
+                }
+                
+        
+        indices = list(index_dict.values())
+        combined = [homophone for i in range(len(indices))]
+        combinations = list(product(*combined))
+
+        for combination in combinations:
+            typo = s        
+            for i in range(len(indices)-1, -1, -1):
+                typo = replace_str_index(typo, indices[i]['index'], indices[i]['length'], combination[i])
+                
+            typos.append(typo)
+
+    return list(set(typos))
+
+
 
